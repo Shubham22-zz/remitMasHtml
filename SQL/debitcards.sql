0adaa91d15fccc4927bf2bab069aacf465510585
shubham <shubhammehta.93@gmail.com>
2015-01-29 07:28:48 +0530

    database files updated

--- SQL/debitcards.sql
+++ SQL/debitcards.sql
@@ -1,6 +1,7 @@
-Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('1223334466667777','344','2021-02-01','9','Rahul');
-Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('3323334465567777','354','2023-03-01','9','Yammi');
-Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('3323114465567777','324','2021-03-01','10','Shubh');
-Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('3323554465567777','384','2022-03-01','10','Manku');
-Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('3323774465567777','394','2025-03-01','10','Manku');
-Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('3323774465567777','397','2025-05-01','11','Shubh');
+Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('1223334466667777','344','2021-02-01','2','Daniela Hernandez');
+Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('3323334465567345','354','2023-03-01','2','Daniela Hernandez');
+Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('3323114465567777','324','2021-03-01','4','Maelis Carraro');
+Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('3323554465564567','384','2022-03-01','2','Daniela Hernandez');
+Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('3323774465567777','394','2025-03-01','7','Steven Pallickal');
+Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('3323774465567777','397','2025-05-01','4','Maelis Carraro');
+Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('3323774465569999','317','2025-06-01','8','Shubham Mehta');
