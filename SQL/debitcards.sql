6e6c88c31aa89e1e8e4329ff57c839f9b9ee5010
shubh <smehtaremit@bibb.dreamhost.com>
2015-03-28 06:55:07 -0700

    debit card table updated

--- SQL/debitcards.sql
+++ SQL/debitcards.sql
@@ -1,7 +1,8 @@
-Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('1223334466667777','344','2021-02-01','2','Daniela Hernandez');
-Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('3323334465567345','354','2023-03-01','2','Daniela Hernandez');
-Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('3323114465567777','324','2021-03-01','4','Maelis Carraro');
-Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('3323554465564567','384','2022-03-01','2','Daniela Hernandez');
-Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('3323774465567777','394','2025-03-01','7','Steven Pallickal');
-Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('3323774465567777','397','2025-05-01','4','Maelis Carraro');
-Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('3323774465569999','317','2025-06-01','8','Shubham Mehta');
+Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('4443334466667777','344','2021-02-01','2','Daniela Hernandez');
+Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('5553334465567345','354','2023-03-01','2','Daniela Hernandez');
+Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('4443114465567777','324','2021-03-01','4','Maelis Carraro');
+Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('5553554465564567','384','2022-03-01','2','Daniela Hernandez');
+Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('5563774465567777','394','2025-03-01','7','Steven Pallickal');
+Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('4435774465567777','397','2025-05-01','4','Maelis Carraro');
+Insert INTO DebitCards(card_no,cvv,expiry,owner_id,card_holder) VALUES ('5673774465569999','317','2025-06-01','8','Shubham Mehta');
+
